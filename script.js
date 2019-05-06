e9ea7e379c6954f01c1059918b592445339b606a
progfay <shun1856shun@gmail.com>
2019-05-06 12:13:33 +0900

    feat: add checkbox and delete button

--- script.js
+++ script.js
@@ -6,7 +6,26 @@ const addTODO = () => {
   if (input.value === '') return
   const todo = document.createElement('li')
   todo.className = 'todo-list-item'
-  todo.innerText = input.value
+
+  const content = document.createElement('div')
+  content.className = 'todo-content'
+  const checkbox = document.createElement('input')
+  checkbox.className = 'todo-checkbox'
+  checkbox.type = 'checkbox'
+  const text = document.createElement('div')
+  text.className = 'todo-text'
+  text.innerText = input.value
+
+  const deleteButton = document.createElement('button')
+  deleteButton.className = 'delete-button'
+  deleteButton.type = 'button'
+  deleteButton.innerText = 'delete'
+
+  content.appendChild(checkbox)
+  content.append(text)
+  todo.appendChild(content)
+  todo.appendChild(deleteButton)
+
   list.appendChild(todo)
   input.value = ''
 }
