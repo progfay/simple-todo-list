cc46685c0de087fb3a96230b67199da02e056ce4
progfay <shun1856shun@gmail.com>
2019-05-07 00:46:22 +0900

    feat: save state of TODO List for localStorage

--- static/script.js
+++ static/script.js
@@ -1,12 +1,26 @@
+/* global localStorage */
+
+const generateID = () => Math.random().toString(36).substr(2, 10)
 const inputElement = document.getElementById('todo-input')
 const buttonElement = document.getElementById('add-button')
 const listElement = document.getElementById('todo-list')
 const template = document.getElementById('todo-template')
 
-const addTODO = (text) => {
-  template.content.querySelector('div.todo-text').innerText = text
+const addTODO = (text, checked) => {
   const todo = document.importNode(template.content, true)
-  const deleteTODO = event => { listElement.removeChild(event.target.parentNode) }
+  const deleteTODO = event => {
+    const { parentNode } = event.target
+    const id = parentNode.querySelector('input.todo-id').value
+    const index = Array.from(listElement.children)
+      .findIndex(child => child.querySelector('input.todo-id').value === id)
+    const todoList = JSON.parse(localStorage.getItem('todoList'))
+    todoList.splice(index, 1)
+    localStorage.setItem('todoList', JSON.stringify(todoList))
+    listElement.removeChild(parentNode)
+  }
+  todo.querySelector('div.todo-text').innerText = text
+  todo.querySelector('input.todo-checkbox').checked = checked
+  todo.querySelector('input.todo-id').value = generateID()
   todo.querySelector('button.delete-button')
     .addEventListener('click', deleteTODO, { once: true, passive: true })
   listElement.appendChild(todo)
@@ -15,16 +29,32 @@ const addTODO = (text) => {
 inputElement.addEventListener('keydown', event => {
   if (event.key !== 'Enter') return
   if (inputElement.value === '') return
-  addTODO(inputElement.value)
+  addTODO(inputElement.value, false)
+  const todoList = JSON.parse(localStorage.getItem('todoList'))
+  todoList.push({ text: inputElement.value, checked: false })
+  localStorage.setItem('todoList', JSON.stringify(todoList))
   inputElement.value = ''
 })
 
 buttonElement.addEventListener('click', () => {
   if (inputElement.value === '') return
   addTODO(inputElement.value)
+  const todoList = JSON.parse(localStorage.getItem('todoList'))
+  todoList.push({ text: inputElement.value, checked: false })
+  localStorage.setItem('todoList', JSON.stringify(todoList))
   inputElement.value = ''
 })
 
-for (const text of ['1st Task', '2nd Task', '3rd Task']) {
-  addTODO(text)
+if (!('todoList' in localStorage)) {
+  localStorage.setItem('todoList', JSON.stringify(
+    [
+      { text: '1st Task', checked: false },
+      { text: '2nd Task', checked: false },
+      { text: '3rd Task', checked: false }
+    ]
+  ))
+}
+
+for (const { text, checked } of JSON.parse(localStorage.getItem('todoList'))) {
+  addTODO(text, checked)
 }
