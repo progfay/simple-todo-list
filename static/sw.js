0397717a7f72c4cb0e2af0b0a5d5f97dc40aa709
progfay <shun1856shun@gmail.com>
2019-05-20 16:28:40 +0900

    chore: update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,3 +1,5 @@
+/* global importScripts, self, workbox */
+
 /**
  * Welcome to your Workbox-powered service worker!
  *
@@ -11,13 +13,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
+    self.skipWaiting()
   }
-});
+})
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -26,40 +28,48 @@ self.addEventListener('message', (event) => {
  */
 self.__precacheManifest = [
   {
-    "url": "images/icons/favicon.ico",
-    "revision": "cf730f14d4a292a2e851507365ed4eef"
+    'url': 'images/icons/favicon.ico',
+    'revision': 'cf730f14d4a292a2e851507365ed4eef'
   },
   {
-    "url": "index.html",
-    "revision": "667ac7b2c5a383ef41044d07ab85d986"
+    'url': 'index.html',
+    'revision': '536984d64dbc30ae8004cb50188df47f'
   },
   {
-    "url": "manifest.json",
-    "revision": "6c7b98ba031dd8be76dbfe6f47b10542"
+    'url': 'manifest.json',
+    'revision': '6c7b98ba031dd8be76dbfe6f47b10542'
   },
   {
-    "url": "script/todo-form.js",
-    "revision": "60982d45ec701762c60835c01009ce1c"
+    'url': 'script/todo-form.js',
+    'revision': '60982d45ec701762c60835c01009ce1c'
   },
   {
-    "url": "script/todo-list-item.js",
-    "revision": "a1137e5a604e186d17b33cc13e90cadc"
+    'url': 'script/todo-list-item.js',
+    'revision': '634d9b6170e8ae97e90fea108f1dfd9d'
   },
   {
-    "url": "script/todo-list.js",
-    "revision": "b1da7bbe0bf6447f29d109cac2b7375c"
+    'url': 'script/todo-list.js',
+    'revision': 'b1da7bbe0bf6447f29d109cac2b7375c'
   },
   {
-    "url": "script/util.js",
-    "revision": "6f054cc5bc51772b63984483229654e6"
+    'url': 'script/util.js',
+    'revision': '6f054cc5bc51772b63984483229654e6'
   },
   {
-    "url": "style.css",
-    "revision": "0ea6950c13ddf5692cb28cd013edb42c"
+    'url': 'style.css',
+    'revision': '9cc7b3487c196f086e493bdb98ce5404'
   },
   {
-    "url": "images/icons/icon-512x512.png",
-    "revision": "c665494f7339f1081378cf2f33f41181"
+    'url': 'images/checkbox.png',
+    'revision': 'eee2913f187c344c8f159cdc2270d34c'
+  },
+  {
+    'url': 'images/checkbox-checked.png',
+    'revision': '1294bac69cf2811f38f63d7f924e0e28'
+  },
+  {
+    'url': 'images/icons/icon-512x512.png',
+    'revision': 'c665494f7339f1081378cf2f33f41181'
   }
-].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+].concat(self.__precacheManifest || [])
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
--- workbox-config.js
+++ workbox-config.js
@@ -2,6 +2,8 @@ module.exports = {
   'globDirectory': 'static',
   'globPatterns': [
     '**/*.{ico,html,json,js,css}',
+    'images/checkbox.png',
+    'images/checkbox-checked.png',
     'images/icons/icon-512x512.png'
   ],
   'globIgnores': [
